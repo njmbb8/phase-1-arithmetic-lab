f6baa07a20e2d5cd830fe3085217fc98d1cc41ad
njmbb8 <nmcgr001@gmail.com>
2021-06-07 14:57:29 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+const num1 = 31;
+const num2 = 2;
+const num3 = 14;
+const num4 = 10;
+const random = Math.ceil(Math.random());
+const max = Math.max(1, 2, 3, 20, 12);
+const multiply = num1 * num2;
+const mod = num3 % num4;
